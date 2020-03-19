a99176eb9fbf161bbb0366667b2f5043708932af
hmson97 <hoangminhson197@gmail.com>
2020-03-19 15:23:17 +0700

    add side menu bar

--- AndroidTV/app/src/main/java/com/example/androidtv/MainActivity.java
+++ AndroidTV/app/src/main/java/com/example/androidtv/MainActivity.java
@@ -9,12 +9,16 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
     Button fragmentBtn1;
     Button fragmentBtn2;
     Button fragmentBtn3;
-
+    ImageButton notiBtn;
+    LinearLayout layout;
+    Button sideTabClosingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,9 @@ public class MainActivity extends Activity {
         fragmentBtn1 = findViewById(R.id.fragment_btn1);
         fragmentBtn2 = findViewById(R.id.fragmen_btn2);
         fragmentBtn3 = findViewById(R.id.fragmen_btn3);
+        notiBtn = findViewById(R.id.btn_noti);
+        layout = findViewById(R.id.side_tab_menu_layout);
+        sideTabClosingButton = findViewById(R.id.sidE_tab_close_btn);
         loadFragment(new Fragment1());
         fragmentBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +46,18 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 loadFragment(new Fragment3());
+            }
+        });
+        notiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setVisibility(View.VISIBLE);
+            }
+        });
+        sideTabClosingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setVisibility(View.GONE);
             }
         });
     }
