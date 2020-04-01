f00d7ddcbd5adab213ba7a135f0316904fae2612
tronghoang <tronghoang2403@gmail.com>
2020-04-01 11:25:55 +0700

    add register form and validate

--- AndroidTV/app/src/main/java/com/example/androidtv/auth/RegisterActivity.kt
+++ AndroidTV/app/src/main/java/com/example/androidtv/auth/RegisterActivity.kt
@@ -1,65 +1,75 @@
 package com.example.androidtv.auth
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
+import android.view.View
+import android.widget.Button
 import com.example.androidtv.MainActivity
 import com.example.androidtv.R
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 import okhttp3.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 
+class RegisterActivity : Activity() {
 
-class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val intentRegister = Intent(this, RegisterActivity::class.java);
+        setContentView(R.layout.activity_register)
         val intentMain = Intent(this, MainActivity::class.java);
-
-        login.setOnClickListener {
+        register.setOnClickListener {
             val passwordPattern =
                 "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,}$".toRegex();
             val usernamePattern =
                 "^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$".toRegex();
+            val emailPattern =
+               "^[a-zA-Z0-9_!#\$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$".toRegex();
             if (username.text.length == 0) {
                 username.requestFocus();
-                username.setError("User name is required")
+                username.setError("User name is required");
             } else
                 if (password.text.length == 0) {
                     password.requestFocus();
                     password.setError("Password is required")
                 } else
-                    if (!password.text.matches(passwordPattern)) {
-                        password.requestFocus();
-                        password.setError("Password is minimum 8 characters, at least one letter, one number and one special character:")
+                    if (email.text.length == 0) {
+                        email.requestFocus();
+                        email.setError("Email is required")
                     } else
-                        if (!username.text.matches(usernamePattern)) {
-                            username.requestFocus();
-                            username.setError("User name doesn't have space and special characters ")
-                        } else {
-                            login(intentMain);
-                        }
-        }
-        register.setOnClickListener {
-            startActivity(intentRegister);
+                        if (!email.text.matches(emailPattern)) {
+                            email.requestFocus();
+                            email.setError("Email isn't valid")
+                        } else
+                            if (!password.text.matches(passwordPattern)) {
+                                password.requestFocus();
+                                password.setError("Password is minimum 8 characters, at least one letter, one number and one special character:")
+                            } else
+                                if (!username.text.matches(usernamePattern)) {
+                                    username.requestFocus();
+                                    username.setError("User name doesn't have space and special characters ")
+                                } else if (password.text.toString() != confirmpassword.text.toString()) {
+                                    confirmpassword.requestFocus();
+                                    confirmpassword.setError("Confirm password isn't same password");
+                                } else{
+                                    register(intentMain);
+                                }
+
         }
     }
 
     @Throws(IOException::class)
-    fun login(main: Intent) {
+    fun register(main:Intent) {
         val MEDIA_TYPE = MediaType.parse("application/json")
-        val url = "http://118.71.224.167:3000/users/login"
+        val url = "http://118.71.224.167:3000/users/register"
         val client = OkHttpClient()
         val postdata = JSONObject()
         try {
             postdata.put("username", username.text)
             postdata.put("password", password.text)
+            postdata.put("email", email.text)
         } catch (e: JSONException) {
             // TODO Auto-generated catch block
             e.printStackTrace()
@@ -87,4 +97,3 @@ class LoginActivity : Activity() {
         })
     }
 }
-
